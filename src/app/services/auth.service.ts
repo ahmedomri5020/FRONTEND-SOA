67eddeb8a06e08458c8db0038fb620f0d87f545c
ahmedomri5020 <ahmed502omri@gmail.com>
2024-11-25 17:41:02 +0100

    last commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,97 +5,97 @@ import { User } from '../model/user.model';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private helper = new JwtHelperService();
 
- private helper = new JwtHelperService();
+  apiURL: string = 'http://localhost:8888/users';
+  token!: string;
 
- apiURL: string = 'http://localhost:8081/users';
- token!: string;
+  public loggedUser!: string;
+  public isloggedIn: Boolean = false;
+  public roles!: string[];
+  public regitredUser: User = new User();
 
- public loggedUser!: string;
- public isloggedIn: Boolean = false;
- public roles!: string[];
- public regitredUser : User = new User();
+  constructor(private router: Router, private http: HttpClient) {}
 
- constructor(private router: Router, private http: HttpClient) { }
-
-
- setRegistredUser(user : User){
-  this.regitredUser=user;
+  setRegistredUser(user: User) {
+    this.regitredUser = user;
   }
-  registerUser(user :User){
-    return this.http.post<User>(this.apiURL+'/register', user,
-    {observe:'response'});
-    }
+  registerUser(user: User) {
+    return this.http.post<User>(this.apiURL + '/register', user, {
+      observe: 'response',
+    });
+  }
 
-    validateEmail(code : string){
-      return this.http.get<User>(this.apiURL+'/verifyEmail/'+code);
-      }
-  getRegistredUser(){
+  validateEmail(code: string) {
+    return this.http.get<User>(this.apiURL + '/verifyEmail/' + code);
+  }
+  getRegistredUser() {
     return this.regitredUser;
+  }
+
+  login(user: User) {
+    return this.http.post<User>(this.apiURL + '/login', user, {
+      observe: 'response',
+    });
+  }
+
+  saveToken(jwt: string) {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.setItem('jwt', jwt);
     }
-    
-
- login(user: User) {
-   return this.http.post<User>(this.apiURL + '/login', user, { observe: 'response' });
- }
-
- saveToken(jwt: string) {
-   if (typeof window !== 'undefined' && window.localStorage) {
-     localStorage.setItem('jwt', jwt);
-   }
-   this.token = jwt;
-   this.isloggedIn = true;
-   this.decodeJWT();
- }
-
- getToken(): string {
-   return this.token;
- }
-
- decodeJWT() {
-  if (this.token == undefined || this.token === '') {
-    return;
-  }
-  try {
-    const decodedToken = this.helper.decodeToken(this.token);
-    this.roles = decodedToken.roles || [];
-    this.loggedUser = decodedToken.sub;
-    this.isloggedIn = true; // Ensure this flag is set after decoding
-  } catch (error) {
-    console.error('Failed to decode JWT:', error);
-    this.logout();
-  }
-}
-
- isAdmin(): boolean {
-  return this.roles && this.roles.includes('ADMIN');
-}
-
- logout() {
-   this.loggedUser = undefined!;
-   this.roles = undefined!;
-   this.token = undefined!;
-   this.isloggedIn = false;
-   localStorage.removeItem('jwt');
-   this.router.navigate(['/login']);
- }
-
- setLoggedUserFromLocalStorage(login: string) {
-   this.loggedUser = login;
-   this.isloggedIn = true;
- }
-
- loadToken() {
-  if (typeof window !== 'undefined' && window.localStorage) {
-    this.token = localStorage.getItem('jwt')!;
+    this.token = jwt;
+    this.isloggedIn = true;
     this.decodeJWT();
   }
-}
 
- isTokenExpired(): Boolean {
-   return this.helper.isTokenExpired(this.token);
- }
+  getToken(): string {
+    return this.token;
+  }
+
+  decodeJWT() {
+    if (this.token == undefined || this.token === '') {
+      return;
+    }
+    try {
+      const decodedToken = this.helper.decodeToken(this.token);
+      this.roles = decodedToken.roles || [];
+      this.loggedUser = decodedToken.sub;
+      this.isloggedIn = true; // Ensure this flag is set after decoding
+    } catch (error) {
+      console.error('Failed to decode JWT:', error);
+      this.logout();
+    }
+  }
+
+  isAdmin(): boolean {
+    return this.roles && this.roles.includes('ADMIN');
+  }
+
+  logout() {
+    this.loggedUser = undefined!;
+    this.roles = undefined!;
+    this.token = undefined!;
+    this.isloggedIn = false;
+    localStorage.removeItem('jwt');
+    this.router.navigate(['/login']);
+  }
+
+  setLoggedUserFromLocalStorage(login: string) {
+    this.loggedUser = login;
+    this.isloggedIn = true;
+  }
+
+  loadToken() {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      this.token = localStorage.getItem('jwt')!;
+      this.decodeJWT();
+    }
+  }
+
+  isTokenExpired(): Boolean {
+    return this.helper.isTokenExpired(this.token);
+  }
 }
--- src/app/services/plat.service.ts
+++ src/app/services/plat.service.ts
@@ -9,14 +9,15 @@ import { PaysWrapper } from '../model/PaysWrapper.model';
 const httpOptions = {
   headers: new HttpHeaders( {'Content-Type': 'application/json'} )
   };
+  const apiURL = 'http://localhost:8888/api';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlatService {
-  imageAPI: string ='http://localhost:8080/api/image'
-
-  apiURL: string = 'http://localhost:8080/api/plats';
-  paysURL: string = 'http://localhost:8080/api/pays';
+  imageAPI: string = apiURL + '/image';
+  apiURLPlats: string = apiURL + '/plats';
+  apiURLPays: string = apiURL + '/pays';
   plats: Plat[] = [];
   pays: Pays[] = [];
 
@@ -24,93 +25,105 @@ export class PlatService {
 
   listePlats(): Observable<Plat[]> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    return this.http.get<Plat[]>(this.apiURL, { headers: httpHeaders });
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    return this.http.get<Plat[]>(this.apiURLPlats, { headers: httpHeaders });
   }
 
   ajouterPlat(plat: Plat): Observable<Plat> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    return this.http.post<Plat>(this.apiURL, plat, { headers: httpHeaders });
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    return this.http.post<Plat>(this.apiURLPlats, plat, {
+      headers: httpHeaders,
+    });
   }
 
   supprimerPlat(id: number): Observable<void> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    const url = `${this.apiURL}/${id}`;
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    const url = `${this.apiURLPlats}/${id}`;
     return this.http.delete<void>(url, { headers: httpHeaders });
   }
 
   consulterPlat(id: number): Observable<Plat> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    const url = `${this.apiURL}/${id}`;
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    const url = `${this.apiURLPlats}/${id}`;
     return this.http.get<Plat>(url, { headers: httpHeaders });
   }
 
   updatePlat(plat: Plat): Observable<Plat> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    const url = `${this.apiURL}/${plat.idPlat}`;
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    const url = `${this.apiURLPlats}/${plat.idPlat}`;
     return this.http.put<Plat>(url, plat, { headers: httpHeaders });
   }
 
   listePays(): Observable<PaysWrapper> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    return this.http.get<PaysWrapper>(this.paysURL, { headers: httpHeaders });
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    return this.http.get<PaysWrapper>(this.apiURLPays, {
+      headers: httpHeaders,
+    });
   }
 
   rechercherParPays(idPays: number): Observable<Plat[]> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    const url = `${this.apiURL}/pays/${idPays}`;
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    const url = `${this.apiURLPlats}/pays/${idPays}`;
     return this.http.get<Plat[]>(url, { headers: httpHeaders });
   }
 
   rechercherParNom(nom: string): Observable<Plat[]> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    return this.http.get<Plat[]>(`${this.apiURL}/search/name?name=${nom}`, { headers: httpHeaders });
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    return this.http.get<Plat[]>(
+      `${this.apiURLPlats}/search/name?name=${nom}`,
+      {
+        headers: httpHeaders,
+      }
+    );
   }
 
   ajouterPays(p: Pays): Observable<Pays> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    return this.http.post<Pays>(this.paysURL, p, { headers: httpHeaders });
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    return this.http.post<Pays>(this.apiURLPays, p, { headers: httpHeaders });
   }
 
   supprimerPays(id: number): Observable<void> {
     let jwt = this.authService.getToken();
-    let httpHeaders = new HttpHeaders({ "Authorization": "Bearer " + jwt });
-    const url = `${this.paysURL}/${id}`;
+    let httpHeaders = new HttpHeaders({ Authorization: 'Bearer ' + jwt });
+    const url = `${this.apiURLPays}/${id}`;
     return this.http.delete<void>(url, { headers: httpHeaders });
   }
-  uploadImage(file: File, filename: string): Observable<Image>{ 
-    const imageFormData = new FormData(); 
-    imageFormData.append('image', file, filename); 
-    const url = `${this.imageAPI + '/upload'}`; 
-    return this.http.post<Image>(url, imageFormData); 
- }
- loadImage(id: number): Observable<Image> { 
-  const url = `${this.imageAPI + '/get/info'}/${id}`; 
-  return this.http.get<Image>(url); 
-} 
-uploadImagePlat(file: File, filename: string, idProd:number): Observable<any>{ 
-  const imageFormData = new FormData(); 
-  imageFormData.append('image', file, filename); 
-  const url = `${this.imageAPI + '/uplaodImagePlat'}/${idProd}`; 
-  return this.http.post(url, imageFormData); 
-}
-supprimerImage(id : number) { 
-  const url = `${this.imageAPI}/delete/${id}`; 
-  return this.http.delete(url, httpOptions); 
-  } 
-  uploadImageFS(file: File, filename: string, idProd : number): Observable<any>{
+  uploadImage(file: File, filename: string): Observable<Image> {
+    const imageFormData = new FormData();
+    imageFormData.append('image', file, filename);
+    const url = `${this.imageAPI + '/upload'}`;
+    return this.http.post<Image>(url, imageFormData);
+  }
+  loadImage(id: number): Observable<Image> {
+    const url = `${this.imageAPI + '/get/info'}/${id}`;
+    return this.http.get<Image>(url);
+  }
+  uploadImagePlat(
+    file: File,
+    filename: string,
+    idProd: number
+  ): Observable<any> {
+    const imageFormData = new FormData();
+    imageFormData.append('image', file, filename);
+    const url = `${this.imageAPI + '/uplaodImagePlat'}/${idProd}`;
+    return this.http.post(url, imageFormData);
+  }
+  supprimerImage(id: number) {
+    const url = `${this.imageAPI}/delete/${id}`;
+    return this.http.delete(url, httpOptions);
+  }
+  uploadImageFS(file: File, filename: string, idProd: number): Observable<any> {
     const imageFormData = new FormData();
     imageFormData.append('image', file, filename);
     const url = `${this.imageAPI + '/uploadFS'}/${idProd}`;
     return this.http.post(url, imageFormData);
   }
-
 }
